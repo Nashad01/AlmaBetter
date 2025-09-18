45fd579726f989f82bf7598d56ff0fa9a49998b5
Mohd Nashad Jilani <147087257+Nashad01@users.noreply.github.com>
2025-09-18 14:16:24 +0000

    Created folder for Chapter 1 and given and added Lesson in the the lesson name

---
+++ Module_2 Design & Analysis of Algorithms/Chapter 1: Introduction to DSA/Lesson: Advanced_Coding_and_Analysis/Try It Yourself/Prob:1_PatternMatching.js
@@ -0,0 +1,7 @@
+/*
+You are a software engineer working on a new search engine. You need to write a function that checks 
+if a given string contains a specific pattern. The pattern could be a word, a phrase, or even a regular 
+expression. The function will be used to filter out search results that do not contain the desired 
+pattern. For example, if the pattern is "dog", the function would return true for the string 
+"I love dogs", but it would return false for the string "I love cats".
+*/
